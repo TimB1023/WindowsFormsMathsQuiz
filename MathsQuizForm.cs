6148d552cb33833606007adc8b22f925556b7876
TimB1023 <39093958+TimB1023@users.noreply.github.com>
2021-08-30 16:03:53 +0200

    Addedd checking if the addition answer is correct

--- MathsQuizForm.cs
+++ MathsQuizForm.cs
@@ -58,6 +58,14 @@ namespace MathsQuiz
             timer1.Start();
         }
 
+        private bool CheckTheAnswer()
+        {
+            if (addend1 + addend2 == sum.Value)
+                return true;
+            else
+                return false;
+        }
+
         private void startButton_Click(object sender, EventArgs e)
         {
             StartTheQuiz();
@@ -66,11 +74,23 @@ namespace MathsQuiz
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (timeLeft > 0)
+            if (CheckTheAnswer())
             {
-                // Display the new time left
-                // by updating the Time Left label.
-                timeLeft = timeLeft - 1;
+                // If CheckTheAnswer() returns true, then the user 
+                // got the answer right. Stop the timer  
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                // If CheckTheAnswer() return false, keep counting
+                // down. Decrease the time left by one second and 
+                // display the new time left by updating the 
+                // Time Left label.
+                timeLeft--;
                 timeLabel.Text = timeLeft + " seconds";
             }
             else
